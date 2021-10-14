bbe73c314675de421306434222e36378cea78fae
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-14 10:17:55 +0200

    create useScript hook

---
+++ src/hooks/useScript.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+const useScript = (url: string, callback: Function) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = url;
+    script.async = true;
+    script.onload = () => {
+      callback();
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [url]);
+};
+
+export default useScript;
